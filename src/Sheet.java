1050778b5a5722e4c34a731addf38e64b6b7718a
jumo9999 <marcjunyent1999@gmail.com>
2020-05-15 13:22:13 +0200

    first SPreadSHEET implementation

--- src/Sheet.java
+++ src/Sheet.java
@@ -6,10 +6,10 @@ public class Sheet {
     private HashMap<String, Cell> table = new HashMap<>();
 
     public Sheet(int size) {
-        createStructure2(size);
+        createStructure(size);
     }
 
-    private void createStructure2(int size) {
+    private void createStructure(int size) {
 
         for(int i=0; i<size; i++){
             for(int j=0; j<size; j++){
@@ -25,4 +25,33 @@ public class Sheet {
         return Character.toString((char) asciiValue).concat(Integer.toString(j + 1));
     }
 
+    public Cell getRef(String s){
+        return table.get(s);
+    }
+
+    public MaybeValue getValue(String name) {
+        Cell currentCell = table.get(name);
+        return currentCell.evaluate();
+       /* if(currentCell.evaluate()==null){
+            return new NoValue();
+        }
+
+        return new SomeValue();*/
+    }
+
+    public void setExpression(String name, Expression expr) {
+        Cell keyCell = table.get(name);
+        keyCell.set(expr);
+        table.put(name, keyCell);
+    }
+
+    public void clearTable(int size) {
+        createStructure(size);
+    }
+/*
+    public void setValue(String name, int value) {
+        Cell keyCell = table.get(name);
+        keyCell.set(new SomeValue(value));
+        table.put(name, keyCell);
+    }*/
 }
--- src/SpreadSheet.java
+++ src/SpreadSheet.java
@@ -4,59 +4,82 @@ public class SpreadSheet {
 
 
     public static MaybeValue get(String name){
-        return null;
+        return SHEET.getValue(name);
     }
 
     public static void put(String name, Expression expr){
+        SHEET.setExpression(name, expr);
     }
 
     public static void put(String name, int value){
-
+        SHEET.setExpression(name, new SomeValue(value));
     }
 
     public static void put(String name, String refName){
-
+        Reference r = new Reference(SHEET.getRef(refName));
+        SHEET.setExpression(name, r);
     }
 
     public static void clear(){
-
+        SHEET.clearTable(SIZE);
     }
+
     //Plus combinations//
     public static Expression plus(Expression expr1, Expression expr2){
-        Operation op = new Plus(expr1, expr2);
-        return op;
+
+        return new Plus(expr1, expr2);
     }
 
     public static Expression plus(Expression expr1, int value2){
-        return null;
+        MaybeValue v2 = new SomeValue(value2);  //Maybe value o Expression
+
+        return new Plus(expr1, v2);
     }
 
     public static Expression plus(Expression expr1, String ref2){
-        return null;
+        Cell cell = SHEET.getRef(ref2);             //Cast de expression??
+
+        return new Plus(expr1, cell.evaluate());
     }
 
     public static Expression plus(int value1, Expression expr2){
-        return null;
+        MaybeValue v1 = new SomeValue(value1);
+
+        return new Plus(v1, expr2);
     }
 
     public static Expression plus(int value1, int value2){
-        return null;
+        MaybeValue v1 = new SomeValue(value1);
+        MaybeValue v2 = new SomeValue(value2);
+
+        return new Plus(v1, v2);
     }
 
     public static Expression plus(int value1, String ref2){
-        return null;
+        MaybeValue v1 = new SomeValue(value1);
+        Cell cell = SHEET.getRef(ref2);             //Cast de expression??
+
+        return new Plus(v1, cell.evaluate());
     }
 
     public static Expression plus(String ref1, Expression expr2){
-        return null;
+        Cell cell = SHEET.getRef(ref1);             //Cast de expression??
+
+        return new Plus(cell.evaluate(), expr2);
     }
 
     public static Expression plus(String ref1, int value2){
-        return null;
+        Cell cell = SHEET.getRef(ref1);             //Cast de expression??
+        MaybeValue v1 = new SomeValue(value2);
+
+        return new Plus(cell.evaluate(), v1);
     }
 
     public static Expression plus(String ref1, String ref2){
-        return null;
+        Cell cell = SHEET.getRef(ref1);             //Cast de expression??
+        Cell cell2 = SHEET.getRef(ref2);             //Cast de expression??
+
+        return new Plus(cell.evaluate(), cell2.evaluate());
     }
 
     //Mult combinations//
