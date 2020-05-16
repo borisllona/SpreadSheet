56bc8e1badc340da4f9bea59058a0fbd8698728b
robertmunne <robertmunne15@gmail.com>
2020-05-16 16:17:59 +0200

    NoValue modificat

--- src/Value/NoValue.java
+++ src/Value/NoValue.java
@@ -2,10 +2,14 @@ package Value;
 
 public class NoValue extends MaybeValue{
 
+   public static final NoValue INSTANCE = new NoValue();
+   private NoValue(){}
+
     //RETORNARA UNA INSTANCIA NO VALUE SINGLETON
     @Override
     public boolean hasValue() {
         /* No se si es necessari */
         return false;
     }
+
 }
